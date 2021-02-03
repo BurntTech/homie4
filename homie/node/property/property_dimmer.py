577bdb413778865d3be03e0149e1773b5d312d51
Patrick Nielsen <pat.github2@burnttech.com>
2021-02-03 13:02:41 -0600

    Update property_dimmer.py
    Trying fix for Dimmer on merge

--- homie/node/property/property_dimmer.py
+++ homie/node/property/property_dimmer.py
@@ -1,9 +1,9 @@
-from .property_float import Property_Float
+from .property_integer import Property_Integer
 
 tags = ["Lighting", "Dimmable"]
 
 
-class Property_Dimmer(Property_Float):
+class Property_Dimmer(Property_Integer):
     def __init__(
         self,
         node,
@@ -36,29 +36,3 @@ class Property_Dimmer(Property_Float):
             tags,
             meta,
         )
-
-    def get_value_from_payload(self, payload):
-        try:
-            return round(int(payload),0)
-        except:
-            return None
-
-    def get_payload_from_value(self, value):
-        try:
-            return round(value,0)
-        except:
-            return None
-
-
-"""
-from .property_integer import Property_Integer
-
-tags = ['Lighting','Dimmable']
-
-class Property_Dimmer(Property_Integer):
-
-    def __init__(self,node, id='dimmer', name = 'Dimmer', settable = True, retained = True, qos=1, unit = '%', data_type= None, data_format = '0:100', value = None, set_value=None, tags=tags, meta={}):
-        
-        super().__init__(node,id,name,settable,retained,qos,unit,data_type,data_format,value,set_value,tags,meta)
-"""
-
